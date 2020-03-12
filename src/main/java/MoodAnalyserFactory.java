41727618c55cfbe919a82604d8b04f09463e0b9e
vishalkarad <vishalkarad777@gmail.com>
2020-03-12 18:40:05 +0530

    Added : New file added MoodAnalyserFactory

--- src/main/java/MoodAnalyserFactory.java
+++ src/main/java/MoodAnalyserFactory.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public class MoodAnalyserFactory {
+
+    // Return constructor instance
+    public static MoodAnalyzer createMoodAnalyzer() {
+        try {
+            Class<?> moodAnalyzer = Class.forName("MoodAnalyzer");
+            Constructor<?> moodConstructor = moodAnalyzer.getConstructor();
+            Object instance = moodConstructor.newInstance();
+            return (MoodAnalyzer) instance;
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
+
