366189a221181f33785adf35df1477a9ca073bd8
vishalkarad <vishalkarad777@gmail.com>
2020-03-13 10:44:43 +0530

    Added : Class name when improper should throw moodAnalysisException

--- src/main/java/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer.java
@@ -2,10 +2,9 @@ import java.util.Objects;
 
 class MoodAnalysisException extends Exception {
     enum MyException_Type{
-        EMPTY,NULL;
+        EMPTY,NULL,CLASS_NOT_FOUND,METHOD_NOT_FOUND;
     }
     MyException_Type type;
-    MyException_Type exceptionMessage;
     MoodAnalysisException(MyException_Type type,String message) {
         super(message);
         this.type=type;
